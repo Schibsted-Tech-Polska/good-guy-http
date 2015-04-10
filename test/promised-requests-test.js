ddd97a545d7bf4f1835d76c2bdb9b17dbc4955aa
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2015-04-10 20:39:21 +0200

    Fixed rejection tests to be cleaner and correct.

--- test/promised-requests-test.js
+++ test/promised-requests-test.js
@@ -1,6 +1,6 @@
 var request = require('request');
-var express = require('express');
 var assert = require('assert');
+var Promise = require('bluebird');
 
 describe("Promised requests", function() {
   var app = require('./test-app')();
@@ -21,47 +21,52 @@ describe("Promised requests", function() {
   });
 
   it("should reject on 4xx HTTP status", function(done) {
-    req(app.url("/return-status/404")).then(function() {
-      done("The promise wasn't rejected.");
-    }).catch(function(err) {
+    expectRejection(req(app.url("/return-status/404"))).then(function(err) {
       assert.equal(err.code, "EHTTP");
       assert.equal(err.message, "HTTP error: status code 404");
       assert.equal(err.status, 404);
       done();
-    });
+    }).catch(done);
   });
 
   it("should reject on 5xx HTTP status", function(done) {
-    req(app.url("/return-status/500")).then(function() {
-      done(new Error("The promise wasn't rejected."));
-    }).catch(function(err) {
+    expectRejection(req(app.url("/return-status/500"))).then(function(err) {
       assert.equal(err.code, "EHTTP");
       assert.equal(err.message, "HTTP error: status code 500");
       assert.equal(err.status, 500);
       done();
-    });
+    }).catch(done);
   });
 
   it("should reject when the request times out", function(done) {
-    req({
+    expectRejection(req({
       url: app.url("/delay-for-ms/2000"),
       timeout: 10
-    }).then(function() {
-      done(new Error("The promise wasn't rejected."));
-    }).catch(function(err) {
+    })).then(function(err) {
       assert.equal(err.code, "ETIMEDOUT");
       done();
-    });
+    }).catch(done);
   });
 
   it("should reject when connection fails", function(done) {
-    req("http://127.0.0.1:1/").then(function() {
-      done(new Error("The promise wasn't rejected."));
-    }).catch(function(err) {
+    expectRejection(req(app.url("http://127.0.0.1:1"))).then(function(err) {
       assert.equal(err.code, "ECONNREFUSED");
       done();
-    });
+    }).catch(done);
   });
 });
 
 
+/**
+ * Inverts a promise: rejections turns into successes (with error as the value),
+ * while successes turn into rejections.
+ */
+function expectRejection(promise) {
+  return new Promise(function(resolve, reject) {
+    promise.then(function() {
+      reject(new Error("The promise wasn't rejected."));
+    }).catch(function(err) {
+      resolve(err);
+    });
+  });
+}
